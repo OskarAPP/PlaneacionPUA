dd3c35861f7239e440d11972d9f0f3c717dd1391
Oscar Perez <al070144@uacam.mx>
2025-08-07 18:07:45 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import './PanelAcceso.jsx'
+// import './PanelAcceso.jsx' // Eliminado: el archivo está en ./Inicio/PanelAcceso.jsx y se importa desde App.jsx
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
